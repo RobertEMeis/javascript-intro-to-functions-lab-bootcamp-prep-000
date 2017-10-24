fbf7f328abf3f0a92afae3429ca7ffe6c3d2909f
Robert Meis <robert.meis@colorado.edu>
2017-10-24 22:03:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+
+return string.toUpperCase();
+
+}
+
+
+function logShout(string) {
+
+console.log(string.toUpperCase());
+
+}
+
+
+sayHiToGrandma(string) {
+
+  if (string === string.toLowerCase()) {
+
+    return "I can't hear you!";
+  }
+
+}
